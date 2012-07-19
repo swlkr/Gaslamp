b2f62206c11afd239ba15fcbc81c2165783ff6cd
Sean Walker <swlkr.rbl@gmail.com>
2012-07-18 22:39:57 -0700

    Initial working version of gaslamp.

---
+++ Rakefile
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+$:.unshift("/Library/RubyMotion/lib")
+require 'motion/project'
+
+Motion::Project::App.setup do |app|
+  # Use `rake config' to see complete project settings.
+  app.name = 'Gaslamp'
+  app.interface_orientations = [:portrait]
+  app.device_family = :iphone
+  app.icons += ["Icon-72.png", "Icon.png", "Icon@2x.png", "Icon-Small-50.png", "Icon-Small.png", "Icon-Small@2x.png", "iTunesArtwork.png"]
+
+end
---
+++ app/gaslamp_view_controller.rb
@@ -0,0 +1,66 @@
+class GaslampViewController < UIViewController
+  def viewDidLoad
+
+    @device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+    flashInput = AVCaptureDeviceInput.deviceInputWithDevice(@device, error: nil)
+    output = AVCaptureVideoDataOutput.alloc.init
+
+    session = AVCaptureSession.alloc.init
+    session.beginConfiguration
+    session.addInput(flashInput)
+    session.addOutput(output)
+    session.commitConfiguration
+    session.startRunning
+
+    # set background image
+    bg_image = UIImage.imageNamed "background.png"
+    bg_image_view = UIImageView.alloc.initWithFrame(self.view.bounds)
+    bg_image_view.image = bg_image
+    view.addSubview(bg_image_view)
+
+    # set us up the button
+    button = UIButton.buttonWithType(UIButtonTypeCustom)
+    button.setImage(UIImage.imageNamed("button.png"), forState:UIControlStateHighlighted)
+    button.frame = [[75, 210],  [170, 170]]
+    button.addTarget(self, action: :buttonIsTapped, forControlEvents:UIControlEventTouchUpInside)
+    view.addSubview(button)
+    
+
+  end
+
+  def buttonIsTapped
+
+    # toggle the light
+    if @device.hasTorch && @device.hasFlash
+      if @device.torchMode == AVCaptureTorchModeOff
+        @device.lockForConfiguration(nil)
+        @device.setTorchMode(AVCaptureTorchModeOn)
+        @device.setFlashMode(AVCaptureFlashModeOn)
+        @device.unlockForConfiguration
+      else
+        @device.lockForConfiguration(nil)
+        @device.setTorchMode(AVCaptureTorchModeOff)
+        @device.setFlashMode(AVCaptureFlashModeOn)
+        @device.unlockForConfiguration
+       end 
+    end
+    
+    #play the sound
+    Dispatch::Queue.concurrent.async do 
+      mainBundle = NSBundle.mainBundle
+      filePath = mainBundle.pathForResource("grim_fart", ofType:"mp3")
+      fileData = NSData.dataWithContentsOfFile(filePath)
+      error = Pointer.new(:object)
+
+      @audio_player = AVAudioPlayer.alloc.initWithData(fileData, error:error)
+
+      unless @audio_player.nil?
+        @audio_player.delegate = self
+        @audio_player.prepareToPlay
+        @audio_player.play
+      end
+
+    end
+  end
+end
